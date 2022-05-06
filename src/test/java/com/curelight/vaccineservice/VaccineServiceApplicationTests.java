287ebe65530a43c1893f92813c291ea2a195e9dd
Sailendra <lsailendra@castlighthealth.com>
2022-05-06 21:38:59 +0530

    Initial commit
    Initial commit

---
+++ src/test/java/com/curelight/vaccineservice/VaccineServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curelight.vaccineservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaccineServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
