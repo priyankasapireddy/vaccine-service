53a4cd5d9d0bb6c319f016382818751fb214d618
Sailendra <lsailendra@castlighthealth.com>
2022-05-19 18:36:30 +0530

    clean up
    clean up

--- src/main/java/com/curelight/vaccineservice/controller/ContactController.java
+++ src/main/java/com/curelight/vaccineservice/controller/ContactController.java
@@ -12,20 +12,6 @@ public class ContactController {
 
     private final ContactService contactService;
 
-    /**
-     * Microsoft Teams meeting
-     * Join on your computer or mobile app
-     * Click here to join the meeting<https://teams.microsoft.com/l/meetup-join/19%3ameeting_ZGE1M2JmMjQtYjczMC00NTM5LTgzZjgtMDVjMmFmMzMzYTlm%40thread.v2/0?context=%7b%22Tid%22%3a%2276a2ae5a-9f00-4f6b-95ed-5d33d77c4d61%22%2c%22Oid%22%3a%2214fbb660-2787-42d2-a7a6-27acb2a7a823%22%7d>
-     * Join with a video conferencing device
-     * teams@vc.capgemini.com
-     * Video Conference ID: 112 718 073 9
-     * Alternate VTC instructions<https://vc.capgemini.com/teams/?conf=1127180739&ivr=teams&d=vc.capgemini.com&ip=194.3.231.206&prefix=55>
-     * Capgemini internal video device can also connect by dialing 55+ VTC Conference ID (55XXXXXXXXXX), External users can refer to “Alternate VTC dialing instructions”
-     * Learn More<https://aka.ms/JoinTeamsMeeting> | Meeting options<https://teams.microsoft.com/meetingOptions/?organizerId=14fbb660-2787-42d2-a7a6-27acb2a7a823&tenantId=76a2ae5a-9f00-4f6b-95ed-5d33d77c4d61&threadId=19_meeting_ZGE1M2JmMjQtYjczMC00NTM5LTgzZjgtMDVjMmFmMzMzYTlm@thread.v2&messageId=0&language=en-US>
-     * ________________________________________________________________________________
-     * @param contactService
-     */
-
     public ContactController(ContactService contactService) {
         this.contactService = contactService;
     }
